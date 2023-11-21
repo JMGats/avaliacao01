12a5feb0140cc4753455a79bfc138c0e3976df2f
CauaMarcus <caua.cardoso600@gmail.com>
2023-11-20 21:38:22 -0300

    MonitorarExercicio

--- app/src/main/java/com/example/avaliacao01/monitor_exercise.java
+++ app/src/main/java/com/example/avaliacao01/monitor_exercise.java
@@ -1,22 +1,114 @@
 package com.example.avaliacao01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 public class monitor_exercise extends AppCompatActivity {
+
     private static final int REQUEST_LAST_LOCATION = 1;
     private static final int REQUEST_LOCATION_UPDATES = 2;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 3;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor_exercise);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        initLocationCallback();
+
+        iniciarMonitoramentoLocalizacao();
     }
+
+    private void initLocationCallback() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+            }
+        };
+    }
+
+    private void iniciarMonitoramentoLocalizacao() {
+        if (checkLocationPermission()) {
+            obterUltimaLocalizacaoConhecida();
+        } else {
+            solicitarPermissaoLocalizacao();
+        }
+    }
+
+    private boolean checkLocationPermission() {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void solicitarPermissaoLocalizacao() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    private void obterUltimaLocalizacaoConhecida() {
+        if (checkLocationPermission()) {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, location -> {
+                        if (location != null) {
+                        }
+                    });
+        }
+    }
+
+
     public void goToMain(View view) {
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_LAST_LOCATION) {
+            if (resultCode == RESULT_OK) {
+            }
+        } else if (requestCode == REQUEST_LOCATION_UPDATES) {
+            if (resultCode == RESULT_OK) {
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                obterUltimaLocalizacaoConhecida();
+            } else {
+                Toast.makeText(this, "Permissão de localização não concedida", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
